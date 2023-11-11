09ba170fac08d3fbbf6a119fbfbe4e26efcb86e0
Kaleb Teshale <kalebteshale72@gmail.com>
2023-11-11 13:30:09 +0300

    feature: added 2 new routes

---
+++ app/route1/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Route1() {
+  return (
+    <div>Hyy route 1</div>
+  )
+}
+
+export default Route1
---
+++ app/route2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Route2() {
+  return (
+    <div>Hyy route 2</div>
+  )
+}
+
+export default Route2
